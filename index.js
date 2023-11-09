0055654c6a36b1bd91e74ac9295d1e7d701d60db
d10sfan <d10sfan@gmail.com>
2023-11-08 19:26:47 -0600

    Fix

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ async function checkDownloadArray(downloadArray, game, issuesFound) {
         } catch(error) {
             issuesFound.push({
                 download_name: download.name,
-                download_status: result.status,
-                download_status_text: result.statusText,
+                download_status: error.status,
+                download_status_text: error.statusText,
                 game_name: game.game_name,
                 game_app_id: game.app_id
             });
